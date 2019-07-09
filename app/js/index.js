01af80ed3d4f4fdd52bf27dee24b52b5cac3b122
Fabiano Taioli <ftaioli@gmail.com>
2019-07-09 10:59:27 +0200

    jsx support

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import tickerView from './views/ticker.js'
 
-const Hello = function (name) {
-    return React.createElement('h1', {}, 'Hello ' + name)
+function tick() {
+  // NO JSX
+  // const tick = React.createElement('div', {},
+  //       [
+  //           React.createElement('h1', {key: 'r1'}, 'Hello, world!'),
+  //           React.createElement('h2', {key: 'r2'}, 'It is' + (new Date().toLocaleTimeString()) + '.')
+  //       ]
+  //   )
+
+  ReactDOM.render(
+      // NO JSX
+      // tick
+    tickerView(new Date().toLocaleTimeString()),
+    document.getElementById('app')
+  )
 }
 
-const view = Hello('World')
-
-ReactDOM.render(view, document.getElementById('app'))
+setInterval(tick, 1000)
---
+++ views/ticker.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function (now) {
+    return <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {now}.</h2>
+    </div>
+}
