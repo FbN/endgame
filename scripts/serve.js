185097450257a034e6f646a200f12be3d510bdcd
Fabiano Taioli <ftaioli@gmail.com>
2019-07-09 10:59:41 +0200

    auto reload

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,7 +1,16 @@
 const browserSyncFactory = require('browser-sync')
 const history = require('connect-history-api-fallback')
+const babel = require("@babel/core")
+const fs = require('fs')
 
 const browserSync = browserSyncFactory.create()
+
+browserSync.watch('app/**/*').on("change", browserSync.reload)
+browserSync.watch('.tmp/**/*').on("change", browserSync.reload)
+browserSync.watch('views/*.jsx', function (event, file) {
+    const {code, map} = babel.transformFileSync(file)
+    fs.writeFileSync('.tmp/js/'+file.replace('.jsx','.js'), code+"/n"+map)
+})
 
 browserSync.init({
     notify: false,
